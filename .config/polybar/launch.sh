0b3c107e2cf8b733c43bcfda152526d342755ed5
Ingar <ingar@osc.no>
2023-02-09 20:26:51 +0000

    init

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+sleep 1
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar top &
+  done
+else
+  polybar top &
+fi
+
+ 
+#polybar bottom;
+
+echo "Bars launched..."
