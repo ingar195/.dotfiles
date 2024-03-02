7db33edc2b3c1514f9511148f49cdfabe632df99
Ingar <ingar@osc.no>
2024-03-02 21:36:51 +0100

    Updated to support wall power

--- .config/polybar/power.sh
+++ .config/polybar/power.sh
@@ -1,5 +1,11 @@
-echo - | awk "{printf \"%.1f\",                     
-$((
-  $(cat /sys/class/power_supply/BAT0/current_now) * 
-  $(cat /sys/class/power_supply/BAT0/voltage_now) 
-)) / 1000000000000 }" ; echo " W"
+current_now=$(cat /sys/class/power_supply/BAT0/current_now)
+voltage_now=$(cat /sys/class/power_supply/BAT0/voltage_now)
+watage_now=$(echo "$current_now * $voltage_now / 1000000000000" | bc)
+
+if [[ $watage_now =~ ^[0-9]+$ ]]; then
+  echo $watage_now W
+else
+  echo "AC"
+fi
+
+
