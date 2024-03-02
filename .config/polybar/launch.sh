25f2daa5974019a4d23b785ad10abbf7027b2e78
Ingar <ingar@osc.no>
2024-03-02 20:29:25 +0100

    reduced sleep

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-sleep 1
+# sleep 1
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
 # Launch bar1 and bar2
 if type "xrandr"; then
